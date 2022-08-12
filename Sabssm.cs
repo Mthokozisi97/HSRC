9174f21a36a16308f31798f3c0ecec3608f8c102
Mthokozisi97 <92888984+Mthokozisi97@users.noreply.github.com>
2022-08-12 17:10:31 +0200

    HSRC

---
+++ Sabssm.cs
@@ -0,0 +1,16 @@
+using systems;
+
+
+namespace Consoleapp
+{
+  class program {
+
+    static void Main( string[] args)
+    {
+
+      HTMlweb  web = new HTMlweb();
+      HTMLDocument document = web.load(C:\Users\msmathe\OneDrive - hsrc.ac.za\Documents\GitHub\Technical%20assessment%20-%20Mthokozisi%20Mathe\Index.html)
+    }
+  }
+
+}
